b40afda384a7ca328c0bd124f7625371c864751b
zhangxiancheng <zhangxiancheng@sinsegye.com.cn>
2025-05-15 11:03:33 +0800

    feat: add logo

--- cmd/mfish/main.go
+++ cmd/mfish/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/lxhanghub/mfish-cli/cmd"
 )
 
 func main() {
+	fmt.Println(` 
+ __  __ _____ _  ______ _    _
+|  \/  |  ___| ||  ___|| |  | |
+| |\/| | |__ | || |___ | |__| |
+| |  | |  __|| ||___  || |--| |
+|_|  |_|_|   |_||_____||_|  |_|
+                               `)
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
