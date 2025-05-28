7d709e7c9a5fa3b3f81b46afcc73c090b779b6d4
zhangxiancheng <zhangxiancheng@sinsegye.com.cn>
2025-05-28 13:12:42 +0800

    feat: update pkg

---
+++ cmd/workit/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/lxhanghub/workit-cli/cmd"
+)
+
+func main() {
+	fmt.Println(` 
+__        __  _______   _ ____    _  ____   _   _______
+\ \      / / |  ___  | | |   ))  | |/ ___| | | |__   __|
+ \ \ /\ / /  | |   | | | |__))   | | |     | |    | |
+  \ V  V /   | |___| | | |_ \__  | | |___  | |    | |
+   \_/\_/    |_______| |_| \___| |_|\____| |_|    |_|
+                               `)
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
